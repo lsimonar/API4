6750ecc14c8af26a1894065bedde9222096202d7
lsimonar <88946419+lsimonar@users.noreply.github.com>
2021-08-21 23:24:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,34 @@ console.log(mongoose.connection.readyState);
 const userSchema = new Schema({
   username: { type: String, required: true },
   id: {type: String, required: false},
-  exercises: {type: Array, required: false}
+  exercises: {type: Array}
 });
 
 var Muser = mongoose.model("User", userSchema);
+
+//&to=todate&limit=number
+app.get('/api/users/:_id/logs',(req,res) =>{
+  const userID = req.params._id;
+  const fromdate = 
+  !req.query.from ? new Date(0) : new Date(req.query.from);
+  const todate = 
+  !req.query.to ? new Date(8640000000000000) : new Date(req.query.to);
+  const limit = !req.query.limit? 99999999999 : Number(req.query.limit);
+
+  Muser.findOne({id: userID}, (err, userfound)=>{
+    var ex_list = userfound.exercises;
+    var short_list = [];
+    
+    for(let i=0; i< ex_list.length; i++){
+      var ex_date = new Date(ex_list[i]['date']);
+      if(todate >= ex_date && ex_date >= fromdate){
+          short_list.push(ex_list[i]);
+      }
+    }
+    var lim_list = short_list.slice(0, limit);
+    res.json({_id: userID, username: userfound.username, from: fromdate.toString().slice(0,15), to: todate.toString().slice(0,15),count: lim_list.length, log: lim_list});
+  }); 
+});
 
 app.get('/api/users', function(req,res){
   Muser.find({}, function(err, userdata){
@@ -48,33 +72,51 @@ app.post('/api/users/:_id/exercises', (req, res)=>{
   else {
     var desc = req.body.description;
     var dur = Number(req.body.duration);
-  }
-  if(!req.body.date) var dat = new Date().toString().slice(0,15);
-  else var dat = new Date(req.body.date).toString().slice(0,15);
+  
+    if(!req.body.date) var dat = new Date().toString().slice(0,15);
+    else var dat = new Date(req.body.date).toString().slice(0,15);
 
-  Muser.findOne({id: userID}, (err,userfound)=>{
-    if(!userfound) res.json({error: "Invalid user ID."});
-    else{
-      var user = userfound.username;
-      var exercisetoset = userfound.exercises;
-      exercisetoset.push({"description":desc,"duration":dur,"date":dat});
-      Muser.findOneAndUpdate({id: userID},{exercises: exercisetoset}, {new: true});
-      res.json({_id: userID, username: user, date: dat, duration: dur,  description: desc});
-    }
-  });
+    Muser.findOne({id: userID}, (err,userfound)=>{
+      if(!userfound) res.json({error: "Invalid user ID."});
+      else{
+        var user = userfound.username;
+        var exercisetoset = userfound.exercises;
+        var count = exercisetoset.push({description: desc, duration: dur, date: dat});
+        Muser.findOneAndUpdate({id: userfound.id},{exercises: exercisetoset}, { upsert: true });
+          userfound.save();  
+        res.json({_id: userID, username: user, date: dat, duration: dur,  description: desc});
+      }
+    });
+  }
 });
 
 app.post('/api/users', function (req,res){
   var In_user = req.body.username;
-  Muser.findOne({}).sort({_id: -1}).exec(function(err,savedusers){
-    if(!savedusers) var userID = 1;
-    else userID = savedusers._id;
-    var newuser = new Muser({username: In_user, id: userID});
+  var newuser = new Muser({username: In_user, id: "", exercises: []});
     //here could do findOneAndUpdate to replace existent urls
-    newuser.save(function(err, data) {
+  newuser.save(function(err, data) {
     if (err) return console.error(err);
+    else{
+      Muser.findByIdAndUpdate({_id: data._id}, {id: data._id}, {upsert: true, new: true}, (err,userfound)=>{
+        userfound.save();
+        res.json({username: userfound.username, _id: userfound.id});
+      }); 
+    }
     });
-    res.json({username: In_user, _id: userID}); 
-  });
+  
 });
 
+
+//app.post('/api/users', function (req,res){
+ // var In_user = req.body.username;
+  //Muser.findOne({}).sort({_id: -1}).exec(function(err,savedusers){
+  //  if(!savedusers) var userID = 1;
+   // else userID = savedusers._id;
+   // var newuser = new Muser({username: In_user, id: userID, exercises: []});
+    //here could do findOneAndUpdate to replace existent urls
+    //newuser.save(function(err, data) {
+    //if (err) return console.error(err);
+    //});
+    //res.json({username: In_user, _id: userID}); 
+  //});
+//});
